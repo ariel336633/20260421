d98b96e00a50f1ca0edd9797bf185e0b85a89d6f
ariel336633 <ariel336633@gmail.com>
2026-04-21 10:27:21 +0800

    1.01_修改攝影機左右顛倒問題

--- sketch.js
+++ sketch.js
@@ -16,8 +16,13 @@ function draw() {
   let videoW = width * 0.6;
   let videoH = height * 0.6;
   
-  // 將影像繪製在畫布正中間
+  // 修正攝影機左右顛倒問題
+  push(); // 保存當前的繪圖狀態
+  translate(width, 0); // 將座標原點移至畫布最右側
+  scale(-1, 1); // 水平翻轉座標軸
+  // 繪製影像，由於座標軸已翻轉，計算出的位置會自動對應到正確的視覺位置
   image(capture, (width - videoW) / 2, (height - videoH) / 2, videoW, videoH);
+  pop(); // 恢復繪圖狀態，避免影響到後續其他的繪圖指令
 }
 
 function windowResized() {
